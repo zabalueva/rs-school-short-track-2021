506a281a95f3436665b813c5df528315ccc778c7
zabalueva <age_eva@mail.ru>
2021-04-08 23:14:22 +0300

    feat: domains complete

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,28 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const doms = {};
+  const arr = [domains].slice();
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      const find = arr[i][j].split('.').reverse();
+      const reg = new RegExp(find[0], 'g');
+      const str = arr.join('.');
+      doms[`.${find[0]}`] = str.match(reg).length;
+
+      if (find.length > 2) {
+        const reg2 = new RegExp(`${find[2]}.${find[1]}.${find[0]}`, 'g');
+
+        doms[`.${find[0]}.${find[1]}.${find[2]}`] = str.match(reg2).length;
+      } else if (find.length > 1) {
+        const reg3 = new RegExp(`${find[find.length - 1]}.${find[0]}`, 'g');
+        doms[`.${find[0]}.${find[find.length - 1]}`] = str.match(reg3).length;
+      }
+    }
+  }
+  return doms;
 }
 
 module.exports = getDNSStats;
