e73e3d28fe94cf3583ac8acedfb2215f125ebdbc
zabalueva <age_eva@mail.ru>
2021-04-11 12:01:11 +0300

    feat: 20 passing

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,48 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const res = [];
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const temp = [];
+      if (j >= 1) {
+        temp.push(matrix[i][j - 1]);
+        if (i + 1 < matrix.length) {
+          temp.push(matrix[i + 1][j - 1]);
+        }
+      }
+      if (i + 1 < matrix.length) {
+        temp.push(matrix[i + 1][j]);
+        if (j + 1 < matrix[i].length) {
+          temp.push(matrix[i + 1][j + 1]);
+        }
+      }
+      if (j + 1 < matrix[i].length) {
+        temp.push(matrix[i][j + 1]);
+      }
+      if (i >= 1) {
+        temp.push(matrix[i - 1][j]);
+        if (j >= 1) {
+          temp.push(matrix[i - 1][j - 1]);
+        }
+        if (j + 1 < matrix[i].length) {
+          temp.push(matrix[i - 1][j + 1]);
+        }
+      }
+      res.push(temp.filter((el) => el === true || el === 1).length);
+    }
+  }
+  let index = Math.ceil(res.length / matrix.length);
+  result.push(res.slice(0, index));
+  while (index < res.length) {
+    result.push(res.slice(index, index + index));
+    index += index;
+  }
+
+  return result;
 }
 
 module.exports = minesweeper;
